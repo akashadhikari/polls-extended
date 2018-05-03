ad2426662e18dc10688f6ff0678240ea3d6b5497
Akash Adhikari <akashsky1313@gmail.com>
2018-05-03 06:15:17 +0545

    Postgres and authentication redirection

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'polls',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -101,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LOGIN_REDIRECT_URL = '/polls'
-LOGOUT_URL = '/'
 
 
 # Internationalization
